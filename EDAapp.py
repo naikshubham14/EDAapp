769ba67241c7034512d8a96c16312f7938418896
naikshubham14 <shubhamnaik321@gmail.com>
2021-08-07 16:09:01 +0530

    Update and rename app.py to EDAapp.py

--- EDAapp.py
+++ EDAapp.py
@@ -8,20 +8,12 @@ from streamlit_pandas_profiling import st_profile_report
 st.markdown('''
 # **The EDA App**
 
-This is the **EDA App** created in Streamlit using the **pandas-profiling** library.
-
-**Credit:** App built in `Python` + `Streamlit` by [Chanin Nantasenamat](https://medium.com/@chanin.nantasenamat) (aka [Data Professor](http://youtube.com/dataprofessor))
-
----
 ''')
 
 # Upload CSV data
 with st.sidebar.header('1. Upload your CSV data'):
     uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-    st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/delaney_solubility_with_descriptors.csv)
-""")
-
+    
 # Pandas Profiling Report
 if uploaded_file is not None:
     @st.cache
